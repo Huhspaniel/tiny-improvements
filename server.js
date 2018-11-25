297e067170583e11e3760f39c42f052c949c66eb
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2018-11-24 23:11:39 -0500

    Connect mongodb and mlab

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 8080;
 
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/tiny-improvements', {
+    useNewUrlParser: true
+})
+const db = mongoose.connection;
+
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, './app/public')));
