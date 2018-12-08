7dd1f0164770129d08265a59ed6cad950a4cb543
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2018-12-08 11:14:43 -0500

    Remove console logs and extra comments

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ class App extends Component {
     kudos: null
   }
   refreshKudos = getKudos.bind(null, kudos => {
-    console.log(kudos[0]);
     this.setState({
       kudos: kudos
     })
@@ -32,69 +31,5 @@ class App extends Component {
     );
   }
 }
-
-// const DOM = {
-//   kudoTitle: document.querySelector('.kudo-title'),
-//   kudoBody: document.querySelector('.kudo-body'),
-//   kudoSubmit: document.querySelector('.kudo-submit'),
-//   kudoTo: document.querySelector('.kudo-to'),
-//   kudoFrom: document.querySelector('.kudo-from'),
-//   kudoList: document.querySelector('.kudo-list')
-// }
-
-// function getKudoInput() {
-//   return {
-//     title: DOM.kudoTitle.value,
-//     body: DOM.kudoBody.value,
-//     to: DOM.kudoTo.value,
-//     from: DOM.kudoFrom.value
-//   };
-// }
-// function clearKudoInput() {
-//   DOM.kudoTitle.value = '';
-//   DOM.kudoBody.value = '';
-// }
-// function clearKudos() {
-//   DOM.kudoList.innerHTML = '';
-// }
-// function clear() {
-//   clearKudoInput();
-//   clearKudos();
-// }
-
-// function renderKudos(kudos) {
-//   if (!kudos[0]) return DOM.kudoList.innerHTML = '<div style=padding:10px>No kudos have been given :(</div>';
-//   kudos.forEach(props => {
-//     DOM.kudoList.innerHTML += Kudo(props);
-//   })
-// }
-// function renderUserSelects(users) {
-//   let user;
-//   users.forEach(({ _id, name }) => {
-//     user = `<option value="${_id}">${name}</option>`;
-//     DOM.kudoFrom.innerHTML += user;
-//     DOM.kudoTo.innerHTML += user;
-//   });
-// }
-
-// getKudos(renderKudos);
-// getUsers(renderUserSelects);
-
-// document.querySelector('.kudo-submit').addEventListener('click', e => {
-//   e.preventDefault();
-//   const kudo = getKudoInput();
-//   if (kudo) {
-//     postKudo(kudo, data => {
-//       if (!data.errors) getKudos(kudos => {
-//         clear();
-//         renderKudos(kudos);
-//       })
-//       else {
-//         console.log(data);
-//       }
-//     });
-//   }
-// });
-
 
 export default App;
